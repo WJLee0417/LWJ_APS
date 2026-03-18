7172084551693dbdcc46f2460ca41740620329fd
WJLee0417 <dnjswns041797@gmail.com>
2026-03-18 14:37:29 +0900

    BOJ_1018 주석 추가

--- 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.java
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.java
@@ -3,11 +3,14 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt();
-		int M = sc.nextInt();
 		
+		int N = sc.nextInt();	// 세로 길이
+		int M = sc.nextInt();	// 가로 길이
+		
+		// 상태 저장 2차원 배열
 		char[][] chess = new char[N][M];
 		
+		// 보드의 각 줄을 입력받아 2차원 배열에 문자(char) 형태로 저장
 		for(int i = 0; i < N; i++) {
 			String line = sc.next();
 			for(int j = 0; j < M; j++) {
@@ -15,13 +18,15 @@ public class Main {
 			}
 		}
 		
+		// 다시 칠해야 하는 정사각형의 최소 개수를 저장할 변수
+		// 최솟값을 찾아야 하므로 초기값은 가장 큰 정수값으로 설정
 		int minCount = Integer.MAX_VALUE;
 		
-		// 2갈래로 분류
-		// 8x8로 자르자.
+		// 전체 보드에서 8x8 크기로 잘라야 하는 모든 경우의 수를 탐색
+		// 8x8 크기 확보해야 하므로 시작점의 범위는 N-8+1, M-8+1 까지
 		for(int i = 0; i < N-8+1; i++) {
 			for(int j = 0; j < M-8+1; j++) {
-				int count = 0;
+				int count = 0;	// 현재 8x8 체스판에서 다시 칠해야 하는 칸 수
 				for(int k = i; k < i+8; k++) {
 					for(int l = j; l < j+8; l++) {
 						if(k % 2 == 0) {
